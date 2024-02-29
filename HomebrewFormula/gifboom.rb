bdc0178fcd55deb5089984bd07847a1edc75378c
austin <tips@cia.lol>
2024-02-29 12:38:57 -0500

    brew

--- HomebrewFormula/gifboom.rb
+++ HomebrewFormula/gifboom.rb
@@ -1,8 +1,8 @@
 class Gifboom < Formula
   desc "Given a list of youtube URLs, convert all to crowd-pleasing animated gifs"
   homepage "https://github.com/hernamesbarbara/gifboom"
-  url "https://github.com/hernamesbarbara/gifboom/releases/download/v1.0.0/gifboom-v1.0.0.tar.gz"
-  sha256 "eb37701cee4cc9915f72206804e0ae8cb7920883458397622a1163a1dabd238d"
+  url "https://github.com/hernamesbarbara/gifboom/releases/download/v1.0.1/gifboom-v1.0.0.tar.gz"
+  sha256 "012f3b1714780071a836031c3eb1dff681c06e31ffd589e8e84a97c567359f1b"
 
   depends_on "ffmpeg", "yt-dlp"
 
