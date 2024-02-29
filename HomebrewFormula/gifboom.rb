5c4282e0713f2cf215480b0aa4730ae8aa681c1e
austin <tips@cia.lol>
2024-02-29 12:10:30 -0500

    stuff

--- HomebrewFormula/gifboom.rb
+++ HomebrewFormula/gifboom.rb
@@ -1,18 +1,18 @@
 class MyTool < Formula
-    desc "A brief description of your tool"
-    homepage "URL to your tool's homepage or GitHub repo"
-    url "URL to a tarball of your tool (you can use GitHub's Releases feature)"
-    sha256 "SHA256 of the tarball"
+    desc "Given a list of youtube URLs, conver all to crowd-pleasing animated gifs"
+    homepage "github.com/hernamesbarbara/gifboom"
+    url "TODO"
+    sha256 "TODO"
   
     depends_on "ffmpeg"
     depends_on "yt-dlp"
   
     def install
-      bin.install "your_script_name" => "mytool"
+      bin.install "gifboom" => "gifboom"
     end
   
     test do
-      system "#{bin}/mytool", "--version"
+      system "#{bin}/gifboom", "--version"
     end
   end
   
