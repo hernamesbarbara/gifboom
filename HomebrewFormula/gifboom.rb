277fffaed7559db56e72391b2053a7d5f7ef65ac
austin <tips@cia.lol>
2024-02-29 12:36:02 -0500

    brew

--- HomebrewFormula/gifboom.rb
+++ HomebrewFormula/gifboom.rb
@@ -4,8 +4,7 @@ class Gifboom < Formula
   url "https://github.com/hernamesbarbara/gifboom/releases/download/v1.0.0/gifboom-v1.0.0.tar.gz"
   sha256 "eb37701cee4cc9915f72206804e0ae8cb7920883458397622a1163a1dabd238d"
 
-  depends_on "ffmpeg"
-  depends_on "yt-dlp"
+  depends_on "ffmpeg", "yt-dlp"
 
   def install
     bin.install "src/gifboom.sh" => "gifboom"
